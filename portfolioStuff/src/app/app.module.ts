2bb0359160e13fa4640550721fc6bfbb7a54afba
Code <code@COCTL-41297.local>
2020-02-05 14:16:54 -0800

    Added in head/main/foot. Begin filling in be4 4pm

--- portfolioStuff/src/app/app.module.ts
+++ portfolioStuff/src/app/app.module.ts
@@ -6,13 +6,21 @@ import { AppComponent } from './app.component';
 import { AboutUsComponent } from './pages/about-us/about-us.component';
 import { FinishedWorkComponent } from './pages/finished-work/finished-work.component';
 import { HomeComponent } from './pages/home/home.component';
+import { ContactComponent } from './pages/contact/contact.component';
+import { HeaderComponent } from './body/header/header.component';
+import { MainComponent } from './body/main/main.component';
+import { FooterComponent } from './body/footer/footer.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     AboutUsComponent,
     FinishedWorkComponent,
-    HomeComponent
+    HomeComponent,
+    ContactComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent
   ],
   imports: [
     BrowserModule,
